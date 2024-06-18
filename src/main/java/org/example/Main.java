0ccd1000737ec8a22cef1c7a80cacf2d57e4aebc
Alexey_Ko <tralex_k@mail.ru>
2024-06-18 23:57:02 +0900

    Sixth commit. Connecting logging to the project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-
 import org.example.compare.UniversityComparator;
 import org.example.compare.StudentComparator;
 import org.example.enums.StudentComparatorEnum;
@@ -16,43 +15,41 @@ import org.example.xls.XlsWriter;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.INFO;
 
 public class Main {
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) throws IOException {
+
+        try {
+            LogManager.getLogManager().readConfiguration(
+                    Main.class.getResourceAsStream("/logging.properties"));
+        } catch (IOException e) {
+            System.err.println("Could not setup logger configuration: " + e.toString());
+        }
+
+        logger.log(INFO, "Application started, Logger configured");
 
         List<University> universities =
                 XlsReader.readUniversities("src/main/resources/universityInfo.xlsx");
         UniversityComparator universityComparator =
                 ComparatorUtil.getUniversityComparator(UniversityComparatorEnum.ID);
         universities.sort(universityComparator);
-        String universitiesJson = JsonUtil.universityListToJson(universities);
-        System.out.println(universitiesJson);
-        List<University> universitiesFromJson = JsonUtil.jsonToUniversityList(universitiesJson);
-        System.out.println(universities.size() == universitiesFromJson.size());
-        universities.forEach(university -> {
-            String universityJson = JsonUtil.universityToJson(university);
-            System.out.println(universityJson);
-            University universityFromJson = JsonUtil.jsonToUniversity(universityJson);
-            System.out.println(universityFromJson);
-        });
 
         List<Student> students =
                 XlsReader.readStudents("src/main/resources/universityInfo.xlsx");
         StudentComparator studentComparator =
                 ComparatorUtil.getStudentComparator(StudentComparatorEnum.AVG_EXAM_SCORE);
         students.sort(studentComparator);
-        String studentsJson = JsonUtil.studentListToJson(students);
-        System.out.println(studentsJson);
-        List<Student> studentsFromJson = JsonUtil.jsonToStudentList(studentsJson);
-        System.out.println(students.size() == studentsFromJson.size());
-        students.forEach(student -> {
-            String studentJson = JsonUtil.studentToJson(student);
-            System.out.println(studentJson);
-            Student studentFromJson = JsonUtil.jsonToStudent(studentJson);
-            System.out.println(studentFromJson);
-        });
 
         List<Statistics> statisticsList = StatisticsUtil.createStatistics(students, universities);
-        XlsWriter.writeXlsStatistics(statisticsList, "src/main/output/statistics1.xlsx");
+        XlsWriter.writeXlsStatistics(statisticsList, "src/main/output/statistics.xlsx");
+
+        logger.log(INFO, "Application finished");
     }
 }
--- src/main/java/org/example/xls/XlsWriter.java
+++ src/main/java/org/example/xls/XlsWriter.java
@@ -11,10 +11,17 @@ import org.example.model.Statistics;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class XlsWriter {
+
+    private static final Logger logger = Logger.getLogger(XlsWriter.class.getName());
+
     public static void writeXlsStatistics(List<Statistics> statisticsList,
                                           String filePath) throws IOException {
+
+        logger.log(Level.INFO, "Excel writing started");
 
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet statisticsSheet = workbook.createSheet("Статистика");
@@ -67,8 +74,14 @@ public class XlsWriter {
             profileCell.setCellStyle(bodyStyle);
         }
 
-        try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
+        try {
+            FileOutputStream outputStream = new FileOutputStream(filePath);
             workbook.write(outputStream);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "New excel file writing failed", e);
+            return;
         }
+
+        logger.log(Level.INFO, "Excel writing finished successfully");
     }
 }
